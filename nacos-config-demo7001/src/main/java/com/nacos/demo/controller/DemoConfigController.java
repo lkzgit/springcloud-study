b2a73ce47be6766b004d767c3857cb7fdcd9754c
likuozhan <lijia9512@163.com>
2021-10-11 23:38:10 +0800

    多环境配置

--- nacos-config-demo7001/src/main/java/com/nacos/demo/controller/DemoConfigController.java
+++ nacos-config-demo7001/src/main/java/com/nacos/demo/controller/DemoConfigController.java
@@ -1,5 +1,9 @@
 package com.nacos.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,8 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
+@RefreshScope //自动刷新
 public class DemoConfigController {
 
+    @Autowired
+    MyProperties myProperties;
 
+    @Value("${user.name}")
+    private String name;
+    @Value("${user.age}")
+     private String age;
+
+    @GetMapping("demo")
+    public String demo(){
+        return "姓名:"+name+"---年龄:"+age;
+    }
+
+    @GetMapping("demo2")
+    public String demo2(){
+        return myProperties.getName()+"--姓名和年龄--"+myProperties.getAge();
+    }
 
 }
---
+++ nacos-config-demo7001/src/main/java/com/nacos/demo/controller/MyProperties.java
@@ -0,0 +1,16 @@
+package com.nacos.demo.controller;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data //lombok
+@Component
+@ConfigurationProperties(prefix = "user")
+public class MyProperties {
+
+    private String name;
+
+    private int age;
+
+}
